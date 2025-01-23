f5238e3bb8680dd74502c68b8dfd503363d73599
Vladimir <veb455@mail.ru>
2025-01-23 11:15:02 +0300

    Header

--- src/Components/Layouts/Header/Header.tsx
+++ src/Components/Layouts/Header/Header.tsx
@@ -2,12 +2,14 @@ import React, { FC, JSX } from "react"
 import style from "./style.module.css"
 import CompanyLogo from "../../UI/CompanyName/CompanyLogo.tsx"
 import RoutingLinks from "../../UI/RoutingLinks/RoutingLinks.tsx"
+import HeaderBtns from "../../UI/HeaderBtns/HeaderBtns.tsx"
 
 const Header:FC = ():JSX.Element => {
     return(
         <header className={style.Header}>
             <CompanyLogo></CompanyLogo>
             <RoutingLinks></RoutingLinks>
+            <HeaderBtns></HeaderBtns>
         </header>
     )
 }
--- src/Components/UI/HeaderBtns/HeaderBtns.tsx
+++ src/Components/UI/HeaderBtns/HeaderBtns.tsx
@@ -1,7 +1,23 @@
 import React, { FC, JSX } from "react";
+import userIcon from "../../../Assets/Icons/user.svg"
+import searchIcon from "../../../Assets/Icons/search.svg"
+import shopBagIcon from "../../../Assets/Icons/shopBag.svg"
+import style from "./style.module.css"
 
-const HeaderBtns:FC = ():JSX.Element => {
-    return(
-        
-    )
-}
+const HeaderBtns: FC = (): JSX.Element => {
+  return (
+    <div className={style.BtnList}>
+      <button className={style.BtnLogin}>
+        <img src={userIcon} alt="" />
+      </button>
+      <button className={style.BtnSearch}>
+        <img src={searchIcon} alt="" />
+      </button>
+      <button className={style.BtnShopper}>
+        <img src={shopBagIcon} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default HeaderBtns
